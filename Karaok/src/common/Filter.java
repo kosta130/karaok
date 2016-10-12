b496d9eef151b68539359bb639e2d4ded7a128bf
Hyunyoung <Hyunyoung@192.168.0.159>
2016-10-12 18:21:37 +0900

    스트럿츠와 ibatis 사용을 위한 기본 환경 세팅

---
+++ Karaok/src/common/Filter.java
@@ -0,0 +1,37 @@
+package common;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class Filter implements javax.servlet.Filter {
+
+	private String encType;
+
+	@Override
+	public void init(FilterConfig config) throws ServletException {
+		encType = config.getInitParameter("EncType");
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		// POST 한글 처리
+		if (encType != null) {
+			request.setCharacterEncoding(encType);
+		} else {
+			request.setCharacterEncoding("UTF-8");
+		}
+		chain.doFilter(request, response);
+	}
+
+	@Override
+	public void destroy() {
+
+	}
+
+}
